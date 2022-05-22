d1e9530d1d9486eefeda44977f24061313267a15
ModPhoenix <ivanchenko.b@gmail.com>
2022-05-23 01:01:19 +0300

    feat: validating email and name in the payload of POST /subscriptions

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -4,6 +4,12 @@ use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
 use tracing_subscriber::{fmt::MakeWriter, layer::SubscriberExt, EnvFilter, Registry};
 
+/// Compose multiple layers into a `tracing`'s subscriber.
+///
+/// # Implementation Notes
+///
+/// We are using `impl Subscriber` as return type to avoid having to spell out the actual
+/// type of the returned subscriber, which is indeed quite complex.
 pub fn get_subscriber<Sink>(
     name: String,
     env_filter: String,
@@ -25,6 +31,7 @@ where
         .with(JsonStorageLayer)
         .with(formatting_layer)
 }
+
 /// Register a subscriber as global default to process span data.
 ///
 /// It should only be called once!
